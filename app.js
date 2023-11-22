acbb2d843b38bc90da7ef8d4afbd84bbec761ac6
Deedar Ahmed <deedarahmed532@gmail.com>
2023-11-22 07:34:58 +0500

    ok

---
+++ app.js
@@ -0,0 +1,21 @@
+// console.log()
+
+console.log("Wellcome to learning hub");
+
+var name = "Deedar ahmed";
+var city = "karachi";
+var country = "Beloved pakistan";
+
+console.log(name);
+console.log(city);
+console.log(country);
+
+// document.write
+
+// br : use for break the line in html 
+
+document.write("Deedar ahmed <br/> <br/>");
+
+document.write("name: ", name, "<br/>");
+document.write("city: ", city, "<br/>");
+document.write("country: ", country);
